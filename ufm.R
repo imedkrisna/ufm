b0453488de1fde7fac7c19bc89efdbf57bdc5945
Krisna Gupta <44009808+imedkrisna@users.noreply.github.com>
2025-03-09 14:13:39 +0700

    a

--- ufm.R
+++ ufm.R
@@ -1,6 +1,6 @@
 setwd("C:/github/ufm") ## This is my WD in my local github desktop
 
-library(tidyverse)
+library(tidyverse) ## Packages
 library(readxl)
 
 dat<-read_excel("data.xlsx",sheet="gabung")|>
@@ -9,3 +9,8 @@ dat<-read_excel("data.xlsx",sheet="gabung")|>
 dat|>filter(missing==1)|>print(n=300)
 
 summary(dat$missing)
+
+## There are around 48% missing, mostly in year 2022 (see the paper).
+
+ea<-dat|>group_by(Province,Year)|>
+  summarise(mean=mean(IDSD))
