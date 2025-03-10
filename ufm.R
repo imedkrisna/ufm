0775de25e96544846517739fdf77152bbc73cc94
Krisna Gupta <imedkrisna@gmail.com>
2025-03-10 15:08:06 +0700

    aw

--- ufm.R
+++ ufm.R
@@ -13,6 +13,34 @@ dat|>filter(missing==1)|>print(n=300)
 
 summary(dat$missing)
 
+## generate some stuff
+
+dat$profit<-dat$REVENUE-dat$EXPENDITURE
+dat$lprofit<-log(dat$profit)
+dat$LREVENUE<-log(dat$REVENUE)
+dat$LEXPENDITURE<-log(dat$EXPENDITURE)
+dat$one<-dat$SINGLE_IMSE/dat$TOTAL_IMSE*100
+dat$lone<-log(dat$one)
+dat$LUNPAID_WORKER<-log(dat$UNPAID_WORKER)
+dat$LTOTAL_IMSE<-log(dat$TOTAL_IMSE)
+dat$rat<-dat$SINGLE_IMSE/dat$TOTAL_IMSE
+dat$lrat<-log(dat$rat)
+dat$LSINGLE_IMSE<-log(dat$SINGLE_IMSE)
+dat$LWAGES<-log(dat$WAGES)
+dat$LIDSD_INST	<-log(dat$	IDSD_INST		)
+dat$LIDSD_INFRA	<-log(dat$	IDSD_INFRA		)
+dat$LIDSD_ICT	<-log(dat$	IDSD_ICT		)
+dat$LIDSD_MACRO	<-log(dat$	IDSD_MACRO		)
+dat$LIDSD_HEALTH	<-log(dat$	IDSD_HEALTH		)
+dat$LIDSD_SKILLS	<-log(dat$	IDSD_SKILLS		)
+dat$LIDSD_PRODUCT	<-log(dat$	IDSD_PRODUCT		)
+dat$LIDSD_LABOUR	<-log(dat$	IDSD_LABOUR		)
+dat$LIDSD_FINANCIAL	<-log(dat$	IDSD_FINANCIAL		)
+dat$LIDSD_MARKET	<-log(dat$	IDSD_MARKET		)
+dat$LIDSD_BUSINESS	<-log(dat$	IDSD_BUSINESS		)
+dat$LIDSD_INNOVATION	<-log(dat$	IDSD_INNOVATION		)
+dat$LIDSD<-log(dat$IDSD)
+
 ## Summary stats of IDSD
 
 idsd<-dat|>select(IDSD_INST,IDSD_INFRA,
@@ -56,6 +84,8 @@ idsd23<-dat|>filter(Year==2023)|>select(IDSD_INST,
                                         IDSD_INNOVATION
 )
 
+d233<-dat|>filter(Year==2023)
+
 datasummary_correlation(idsd,output = 'tab/corr.html')
 datasummary_correlation(idsd22,output = 'tab/corr22.html')
 datasummary_correlation(idsd23,output = 'tab/corr23.html')
@@ -65,40 +95,7 @@ datasummary(IDSD_INST+IDSD_INFRA+IDSD_ICT+IDSD_MACRO+IDSD_HEALTH+IDSD_SKILLS+IDS
               factor(Year)*(Mean+SD+N+Histogram),
             data=dat,output='tab/idsdsum.docx')
 
-## summary stats per province
 
-datasummary(IDSD_INST+IDSD_INFRA+IDSD_ICT+IDSD_MACRO+IDSD_HEALTH+IDSD_SKILLS+IDSD_PRODUCT+
-              IDSD_LABOUR+IDSD_FINANCIAL+IDSD_MARKET+IDSD_BUSINESS+IDSD_INNOVATION~
-              factor(Year)*(Mean+SD+N+Histogram),
-            data=datp,output='tab/idsdsump.docx')
-
-## generate some stuff
-
-dat$profit<-dat$REVENUE-dat$EXPENDITURE
-dat$lprofit<-log(dat$profit)
-dat$LREVENUE<-log(dat$REVENUE)
-dat$LEXPENDITURE<-log(dat$EXPENDITURE)
-dat$one<-dat$SINGLE_IMSE/dat$TOTAL_IMSE*100
-dat$lone<-log(dat$one)
-dat$LUNPAID_WORKER<-log(dat$UNPAID_WORKER)
-dat$LTOTAL_IMSE<-log(dat$TOTAL_IMSE)
-dat$rat<-dat$SINGLE_IMSE/dat$TOTAL_IMSE
-dat$lrat<-log(dat$rat)
-dat$LSINGLE_IMSE<-log(dat$SINGLE_IMSE)
-dat$LWAGES<-log(dat$WAGES)
-dat$LIDSD_INST	<-log(dat$	IDSD_INST		)
-dat$LIDSD_INFRA	<-log(dat$	IDSD_INFRA		)
-dat$LIDSD_ICT	<-log(dat$	IDSD_ICT		)
-dat$LIDSD_MACRO	<-log(dat$	IDSD_MACRO		)
-dat$LIDSD_HEALTH	<-log(dat$	IDSD_HEALTH		)
-dat$LIDSD_SKILLS	<-log(dat$	IDSD_SKILLS		)
-dat$LIDSD_PRODUCT	<-log(dat$	IDSD_PRODUCT		)
-dat$LIDSD_LABOUR	<-log(dat$	IDSD_LABOUR		)
-dat$LIDSD_FINANCIAL	<-log(dat$	IDSD_FINANCIAL		)
-dat$LIDSD_MARKET	<-log(dat$	IDSD_MARKET		)
-dat$LIDSD_BUSINESS	<-log(dat$	IDSD_BUSINESS		)
-dat$LIDSD_INNOVATION	<-log(dat$	IDSD_INNOVATION		)
-dat$LIDSD<-log(dat$IDSD)
 
 
 ## Aggregate by province year
@@ -106,9 +103,30 @@ dat$LIDSD<-log(dat$IDSD)
 datp<-dat|>group_by(Province,Year)|>
   summarise_if(is.numeric,mean,na.rm=TRUE)
 
-d233<-dat|>filter(Year==2023)
+## summary stats per province
 
+datasummary(IDSD_INST+IDSD_INFRA+IDSD_ICT+IDSD_MACRO+IDSD_HEALTH+IDSD_SKILLS+IDSD_PRODUCT+
+              IDSD_LABOUR+IDSD_FINANCIAL+IDSD_MARKET+IDSD_BUSINESS+IDSD_INNOVATION~
+              factor(Year)*(Mean+SD+N+Histogram),
+            data=datp,output='tab/idsdsump.docx')
 
+macro<-read_excel("macro.xlsx",sheet="gp")
+datp<-datp|>full_join(macro,by=c("Province","Year"))
+datp$gp<-datp$GRP/datp$pop
+
+## graph
+
+datp|>ggplot(aes(x=GRP,y=IDSD_INST,color=factor(Year)))+geom_point(size=1.1)+
+  theme_classic()
+ggsave("fig/fig1.png")
+
+datp|>ggplot(aes(x=pop,y=IDSD_INST,color=factor(Year)))+geom_point(size=1.1)+
+  theme_classic()
+ggsave("fig/fig2.png")
+
+datp|>ggplot(aes(x=gp,y=IDSD_INST,color=factor(Year)))+geom_point(size=1.1)+
+  theme_classic()
+ggsave("fig/fig3.png")
 
 ## Regression
 
